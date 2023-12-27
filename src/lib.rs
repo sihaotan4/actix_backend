610c4e15cda1116b92169b41729aefd53c518db7
sihaotan4 <34644190+sihaotan4@users.noreply.github.com>
2023-12-27 22:02:37 +0800

    add postgres docker init

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,22 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(lst: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
         )
         .listen(lst)?
         .run();
     Ok(server)
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
 }
